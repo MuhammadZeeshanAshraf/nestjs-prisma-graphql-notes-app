198402c295e74e3c38137731b80ef56cb0b27882
Zeeshan Ashraf <m.zeeshan.ashraf.633@gmail.com>
2024-01-24 00:48:11 +0500

    chore: update sample env and joi env validation schema

--- src/configuration/validation/env.validation.ts
+++ src/configuration/validation/env.validation.ts
@@ -3,5 +3,7 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production').required(),
   DATABASE_URL: Joi.string().required(),
-  PORT: Joi.number().port().required()
+  PORT: Joi.number().port().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES: Joi.string().required()
 });
